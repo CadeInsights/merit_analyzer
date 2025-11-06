c2ac48c9693e9209d02bb64c7b101adb1b4afa95
RealNickSheero <nick@tensorsense.com>
2025-11-05 16:52:24 -0800

    fixes

---
+++ k.py
@@ -0,0 +1,3 @@
+from anthropic import Anthropic, AnthropicBedrock
+
+print(AnthropicBedrock().models.list)
--- src/merit_analyzer/engines/t.py
+++ src/merit_analyzer/engines/t.py
@@ -1,29 +1,15 @@
-import inspect
-from typing import get_type_hints, List, Dict, Literal, TypedDict
-from rich import print
-from pydantic import BaseModel, TypeAdapter, create_model
-from enum import Enum
-
-class Exec(Enum):
-    CEO = "ceo"
-    CTO = "cto"
-    CMO = "cmo"
-
-class Person(BaseModel):
-    name: str
-    age: int
-
-class Org(BaseModel):
-    employees: List[Person]
-    org_name: str
-    execs: Dict[Exec, Person]
+from anthropic import Anthropic, AnthropicBedrock, AnthropicVertex
 
 
-def t(org: str, action: Literal["Yes", "No"]):
-    return ""
-
-sig = inspect.signature(t)
-hints = get_type_hints(t)
-model = create_model("Args", **{name: (tp, ...) for name, tp in hints.items()})
-
-print(model.model_json_schema())
+c = AnthropicBedrock()
+message = c.messages.create(
+    max_tokens=1024,
+    messages=[
+        {
+            "role": "user",
+            "content": "Hello!",
+        }
+    ],
+    model="anthropic.claude-sonnet-4-5-20250929-v1:0",
+)
+print(message.model_dump_json(indent=2))
